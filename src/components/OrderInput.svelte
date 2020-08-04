0b0f0dcd709eee9fd525018b3b371fafd41d1ac4
Paul Krisadaolarn <pauly.k@gmail.com>
2020-08-04 12:13:06 +0700

    star ton scan component

--- src/components/OrderInput.svelte
+++ src/components/OrderInput.svelte
@@ -1,27 +1,28 @@
 <script>
-  import {  data } from '../util/data'
-  import { debounce } from '../util/util'
+    import { onMount } from 'svelte'
 
-  let itemId = null
-  let entry = undefined
-
-  function getItem() {
-    console.log('get time, ', itemId)
-    entry = data.find(x => x.id === parseInt(itemId))
-    console.log('entry', entry)
-    itemId = null
-  }
 </script>
 
-<input type="text" bind:value={itemId} on:keypress={debounce(getItem, 200)}>
-
-{#if entry} 
-  <div class="entry">{entry.name}</div>
-{/if}
-
 <style>
-  .entry {
-    font-size: 2.5rem;
-    color: rgb(15, 15, 113);
-  }
+    .page {
+        max-width: 600px;
+        margin: 0 auto;
+        border: 1px solid #444;
+        min-height: 300px;
+        display: flex;
+        justify-content: center;
+        align-items: start;
+    }
+
+    
+
 </style>
+
+<div class="page" >
+    Listen for scanner input and find the appropriate entry in a database.
+    Use debounce to delay lookup until all characters have been entered.
+    Ability to look up multiple entries and add them to a list.
+    Stop listening for scans once at 3 entries.
+    The component that listens for a scan should be an input element that auto fucesses one mount.
+
+</div>
--- src/util/util.js
+++ src/util/util.js
@@ -1,13 +1,15 @@
-export const debounce = (func, wait) => {
-  let timeout;
 
-  return function executedFunction(...args) {
-    const later = () => {
-      clearTimeout(timeout);
-      func(...args);
-    };
 
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-  };
-};
+export function debounce(fn, delay = 200) {
+  let timer
+  let that = this
+  return function(...args) {
+    if (timer) {
+      clearTimeout(timer)
+      timer = null
+    }
+    timer  = setTimeout(() => {
+      return fn.apply(that, args)
+    }, delay);
+  }
+}
